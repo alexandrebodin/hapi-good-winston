b136b38a9896891e4e0819bfeefd01df8d3df43b
Alexandre BODIN <bodin.alex@gmail.com>
2016-06-23 15:14:59 +0200

    adding tests

---
+++ test/fixtures.js
@@ -0,0 +1,89 @@
+const log = {
+    event: 'log',
+    timestamp: 1458264810957,
+    tags: ['log', 'info'],
+    data: 'my data',
+    pid: 64291,
+};
+
+const ops = {
+    event: 'ops',
+    timestamp: 1458264810957,
+    host: 'localhost',
+    pid: 64291,
+    os: {
+        load: [1.650390625, 1.6162109375, 1.65234375],
+        mem: { total: 17179869184, free: 8190681088 },
+        uptime: 704891,
+    },
+    proc: {
+        uptime: 6,
+        mem: {
+            rss: 30019584,
+            heapTotal: 18635008,
+            heapUsed: 9989304,
+        },
+        delay: 0.03084501624107361,
+    },
+    load: {
+        requests: {},
+        concurrents: {},
+        responseTimes: {},
+        listener: {},
+        sockets: { http: {}, https: {} },
+    },
+};
+
+const response = {
+    event: 'response',
+    timestamp: 1458264810957,
+    id: '1458264811279:localhost:16014:ilx17kv4:10001',
+    instance: 'http://localhost:61253',
+    labels: [],
+    method: 'post',
+    path: '/data',
+    query: {
+        name: 'adam',
+    },
+    responseTime: 150,
+    statusCode: 200,
+    pid: 16014,
+    httpVersion: '1.1',
+    source: {
+        remoteAddress: '127.0.0.1',
+        userAgent: 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/49.0.2623.87 Safari/537.36',
+        referer: 'http://localhost:61253/',
+    },
+};
+
+const request = {
+    event: 'request',
+    timestamp: 1458264810957,
+    tags: ['user', 'info'],
+    data: 'you made a request',
+    pid: 64291,
+    id: '1419005623332:new-host.local:48767:i3vrb3z7:10000',
+    method: 'get',
+    path: '/',
+};
+
+const error = {
+    event: 'error',
+    timestamp: 1458264810957,
+    id: '1419005623332:new-host.local:48767:i3vrb3z7:10000',
+    url: 'http://localhost/test',
+    method: 'get',
+    pid: 64291,
+    error: {
+        message: 'Just a simple error',
+        stack: 'Error: Just a simple Error',
+    },
+};
+
+export default {
+    request,
+    error,
+    response,
+    ops,
+    log,
+};
--- test/index.js
+++ test/index.js
@@ -1,5 +1,124 @@
-var test = require('tape');
+import test from 'tape';
+import handlers from '../lib/handlers';
+import fixtures from './fixtures';
+import { logLvl, log, goodWinston } from '..';
 
-test('timing test', t => {
+test('check logLevels', t => {
+    t.plan(5);
 
-})
+    t.equal(logLvl.ops, 'debug');
+    t.equal(logLvl.log, 'info');
+    t.equal(logLvl.error, 'error');
+    t.equal(logLvl.request, 'info');
+    t.equal(logLvl.response, 'info');
+});
+
+test('checking logging function', t => {
+    t.plan(9);
+
+    const genFakeLogger =  (expectedLvl, expectedMsg, expectedMeta) => {
+        return {
+            log(lvl, msg, meta) {
+                t.equal(lvl, expectedLvl);
+                t.equal(msg, expectedMsg);
+                t.equal(meta, expectedMeta);
+            },
+        };
+    };
+
+    log(genFakeLogger('ops', 'my message', 'my meta'), 'ops', {
+        msg: 'my message',
+        meta: 'my meta',
+    });
+
+    log(genFakeLogger('ops', 'my message', ''), 'ops', {
+        msg: 'my message',
+    });
+
+    log(genFakeLogger('ops', '', ''), 'ops', {});
+});
+
+test('checking stream instance', t => {
+    t.plan(1);
+
+    const fakeLogger = { log: () => {} };
+    const stream = goodWinston(fakeLogger);
+
+    t.equal(typeof stream.pipe, 'function');
+});
+
+test('checking throws if not logger or not log method on logger', t => {
+    t.plan(6);
+
+    const err = new Error('You must pass a valid winston logger');
+    t.throws(goodWinston, err);
+    t.throws(goodWinston.bind({}), err);
+    t.throws(goodWinston.bind(() => {}), err);
+    t.throws(goodWinston.bind({log: 'azd'}), err);
+    t.throws(goodWinston.bind({log: 1}), err);
+    t.throws(goodWinston.bind({log: {}}), err);
+});
+
+test('checking instance safety of loggers', t => {
+    t.plan(2);
+
+    const s1 = goodWinston({ log: () => {}});
+    const s2 = goodWinston({ log: () => {}});
+
+    t.equal(Object.getPrototypeOf(s1), Object.getPrototypeOf(s2));
+    t.notEqual(s1.logger, s2.logger);
+});
+
+test('testing ops handler', t => {
+    t.plan(1);
+
+    const expected = {
+        msg: 'memory: 29Mb, uptime: 6s, load: 1.650390625,1.6162109375,1.65234375',
+    };
+
+    t.deepEqual(handlers[fixtures.ops.event](fixtures.ops), expected);
+});
+
+test('testing log handler', t => {
+    t.plan(1);
+
+    const expected = {
+        msg: `[log,info] my data`,
+    };
+
+    t.deepEqual(handlers[fixtures.log.event](fixtures.log), expected);
+});
+
+test('testing error handler', t => {
+    t.plan(1);
+
+    const expected = {
+        meta: {
+            message: 'Just a simple error',
+            stack: 'Error: Just a simple Error',
+        },
+    };
+
+    t.deepEqual(handlers[fixtures.error.event](fixtures.error), expected);
+});
+
+
+test('testing request handler', t => {
+    t.plan(1);
+
+    const expected = {
+        msg: `[user,info] GET / you made a request`,
+    };
+
+    t.deepEqual(handlers[fixtures.request.event](fixtures.request), expected);
+});
+
+test('testing response handler', t => {
+    t.plan(1);
+
+    const expected = {
+        msg: `http://localhost:61253: POST /data {"name":"adam"} 200 (150ms)`,
+    };
+
+    t.deepEqual(handlers[fixtures.response.event](fixtures.response), expected);
+});
